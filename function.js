241405e9035b9acce905b65b55cf49f282133340
github Roxas0428 <daichan04280507@icloud.com>
2024-07-04 10:12:44 +0900

    高階関数

--- function.js
+++ function.js
@@ -9,19 +9,49 @@
 // }
 // greet("Ken","Fukuyama")
 
-function repeat(str, numTimes) {
-  let result = " ";
-  for (let i = 0; i < numTimes; i++) {
-    result += str;
-  }
+// function repeat(str, numTimes) {
+//   let result = " ";
+//   for (let i = 0; i < numTimes; i++) {
+//     result += str;
+//   }
 
-  console.log(result);
-}
+//   console.log(result);
+// }
 
-function add(x, y) {
-  if (typeof x !== "number" || typeof y !== "number") {
-    return false;
-  }
-  const sum = x + y;
-  return sum;
-}
+// function add(x, y) {
+//   if (typeof x !== "number" || typeof y !== "number") {
+//     return false;
+//   }
+//   const sum = x + y;
+//   return sum;
+// }
+
+// function callTwice(func) {
+//   func();
+//   func();
+// }
+
+// function callTenItems(f) {
+//   for (let i = 0; i < 10; i++) {
+//     f();
+//   }
+// }
+
+// function rollDie() {
+//   const roll = Math.floor(Math.random() * 6) + 1;
+//   console.log(roll);
+// }
+// callTwice(rollDie);
+
+// function makeBetweenFunc(min, max) {
+//   return function (num) {
+//     return num >= min && num <= max;
+//   };
+// }
+
+// makeBetweenFunc(50,100)
+
+// function isBetween(num){
+//   return num >= 50 && num <= 100
+// }
+
