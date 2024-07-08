115de2040fcafd71d1d1e06f04a870c6e7039c53
github Roxas0428 <daichan04280507@icloud.com>
2024-07-08 17:36:00 +0900

    区切り

---
+++ matome/function.js
@@ -0,0 +1,79 @@
+// function singSong() {
+//   console.log("ど");
+//   console.log("れ");
+//   console.log("み");
+// }
+
+// function greet(firstName, lastName) {
+//   console.log(`Hi, ${firstName}  ${firstName[0]}`);
+// }
+// greet("Ken","Fukuyama")
+
+// function repeat(str, numTimes) {
+//   let result = " ";
+//   for (let i = 0; i < numTimes; i++) {
+//     result += str;
+//   }
+
+//   console.log(result);
+// }
+
+// function add(x, y) {
+//   if (typeof x !== "number" || typeof y !== "number") {
+//     return false;
+//   }
+//   const sum = x + y;
+//   return sum;
+// }
+
+// function callTwice(func) {
+//   func();
+//   func();
+// }
+
+// function callTenItems(f) {
+//   for (let i = 0; i < 10; i++) {
+//     f();
+//   }
+// }
+
+// function rollDie() {
+//   const roll = Math.floor(Math.random() * 6) + 1;
+//   console.log(roll);
+// }
+// callTwice(rollDie);
+
+// function makeBetweenFunc(min, max) {
+//   return function (num) {
+//     return num >= min && num <= max;
+//   };
+// }
+
+// makeBetweenFunc(50,100)
+
+// function isBetween(num){
+//   return num >= 50 && num <= 100
+// }
+
+// const add = (x, y) => {
+//   return x + y;
+// };
+// const square = num => {
+//   return num * num;
+// };
+
+// const rollDie = () => {
+//   return Math.floor(Math.random() * 6) + 1;
+// };
+
+// setTimeout(() => {
+//   console.log("やほーー");
+// }, 3000);
+
+// const id = setInterval(() => {
+//   console.log(Math.random());
+// }, 2000);
+
+// clearInterval(id);
+
+
