9addd860b1b47f648fbf4869e38199de3c302cc6
github Roxas0428 <daichan04280507@icloud.com>
2024-07-22 10:37:24 +0900

    js

--- httpReq/app.js
+++ httpReq/app.js
@@ -23,14 +23,57 @@
 //   .catch((err) => {
 //     console.log("error", err);
 //   });
-fetch("https://swapi.dev/api/people/1/")
-  .then((res) => {
-    console.log("RESOLVE", res);
-    return res.json();
-  })
-  .then((data) => {
-    console.log(data);
-  })
-  .catch((err) => {
-    console.log("error", err);
-  });
+// fetch("https://swapi.dev/api/people/1/")
+//   .then((res) => {
+//     console.log("RESOLVE", res);
+//     return res.json();
+//   })
+//   .then((data) => {
+//     console.log(data);
+//   })
+//   .catch((err) => {
+//     console.log("error", err);
+//   });
+
+// const loadStarWarsPeople = async () => {
+//   try {
+//     const res = await fetch("https://swapi.dev/api/people/1/");
+//     const data = await res.json();
+//     console.log(data);
+//     const res2 = await fetch("https://swapi.dev/api/people/2/");
+//     const data2 = await res2.json();
+//     console.log(data2);
+//   } catch (e) {
+//     console.log("エラー", e);
+//   }
+// };
+// loadStarWarsPeople();
+
+// const getStarWarsPerson = async(id)=>{
+//     const res = await axios.get(`https://swapi.dev/api/people/1/${id}`)
+//     console.log(res.data)
+// }
+
+const jokes = document.querySelector("#jokes");
+const button = document.querySelector("button");
+
+const getDadJoke = async () => {
+  try {
+    const config = {
+      headers: {
+        Accept: "application/json",
+      },
+    };
+    const res = await axios.get("https://icanhazdadjoke.com/", config);
+    return res.data.joke;
+  } catch (e) {
+    return "No Jokes Sorry";
+  }
+};
+const addNewJoke = async () => {
+  const jokeText = await getDadJoke();
+  const newLi = document.createElement("Li");
+  newLi.append(jokeText);
+  jokes.append(newLi);
+};
+button, addEventListener("click", addNewJoke);
---
+++ object/app.js
@@ -0,0 +1,26 @@
+// function Color(r, g, b) {
+//   this.r = r;
+//   this.g = g;
+//   this.b = b;
+// }
+// Color.prototype.rgb = function () {
+//   const { r, g, b } = this;
+//   return `rgb${r},${g},${b}`;
+// };
+
+// class Color {
+//   constructor(r, g, b, name) {
+//     this.r = r;
+//     this.g = g;
+//     this.b = b;
+//     this.name = name;
+//   }
+//   rgb() {
+//     const { r, g, b } = this;
+//     return `rgb${r},${g},${b}`;
+//   }
+// }
+
+// const c1 = new Color(255, 67, 89, "unko");
+// const c2 = new Color(255, 255, 78, "unk");
+
